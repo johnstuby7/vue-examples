e1641e43aa644a8688bf9c7d0f04a71db1d51d41
John Stuby <johnstuby7@gmail.com>
2023-11-01 15:23:55 -0600

    Added in the logic to return information from the omdb api, currently i have a issue with my api key, so it is not returning movies. Added in styling for most returned data if it worked

--- MovieApp/vue-movie-picker/src/views/HomeView.vue
+++ MovieApp/vue-movie-picker/src/views/HomeView.vue
@@ -13,24 +13,54 @@
       <!-- <router-link to="/movies" /> -->
     </div>
 
-    <form @submit.prevent="" class="search-box">
-      <input type="text" placeholder="What are you looking for?">
+    <form @submit.prevent="SearchMovies()" class="search-box">
+      <input type="text" placeholder="What are you looking for?" v-model="search">
       <input type="submit" value="Search">
     </form>
 
-    <div class="movies-list">MOVIES</div>
+    <div class="movies-list">
+      <div class="movie" v-for="movie in movies" :key="movie.imdbID">
+        <router-link :to="'/movie/' + movie.imdbID">
+          <div class="product-image">
+            <img :src="movie.Poster" alt="Movie Poster" />
+            <div class="type">{{ movie.Type }}</div>
+          </div>
+          <div class="detail">
+            <p class="y">{{ movie.Year }}</p>
+            <h3>{{ movie.Title }}</h3>
+          </div>
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import { ref } from "vue";
+import env from '@/env.js'
 
 export default {
   setup() {
-    const search = ref("")
+    const search = ref("");
+    const movies = ref([]);
+
+    // The fetch is having issues using the apikey, will need to work on it
+    const SearchMovies = () => {
+      if (search.value != "") {
+        fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
+          .then(response => response.json())
+          .then(data => {
+            movies.value = data.Search;
+            search.value = "";
+            console.log(movies.value)
+          })
+      }
+    }
 
     return {
-      search
+      search,
+      movies,
+      SearchMovies
     }
   }
 }
@@ -113,6 +143,45 @@ export default {
 
           &:active {
             background-color: #3BB070;
+          }
+        }
+      }
+    }
+  }
+
+  .movies-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px 8px;
+
+    .movie {
+      max-width: 50%;
+      flex: 1 1 50%;
+      padding: 16px 8px;
+
+      .movie-link {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+
+        .product-image {
+          position: relative;
+
+          display: block img {
+            display: block;
+            width: 100%;
+            height: 275px;
+            object-fit: cover;
+          }
+
+          .type {
+            position: absolute;
+            padding: 8px 16px;
+            background-color: #42B883;
+            color: #fff;
+            bottom: 16px;
+            left: 0px;
+            text-transform: capitalize;
           }
         }
       }
