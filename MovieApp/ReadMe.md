2fab4ee3783ca0d9cbb30f51c7cf1ef691470f64
John Stuby <johnstuby7@gmail.com>
2023-11-01 14:46:02 -0600

    Added in new page for movie details, added link to opendb for the specific main image naruto, currently it is not returning correctly, will need to figure it out

--- MovieApp/ReadMe.md
+++ MovieApp/ReadMe.md
@@ -6,3 +6,46 @@
 - create a env.js file in src to hold the omdb api key, this is the movie api db
 - remove the components and assets folder and update app to just have the routerview
 - Add <router-link to="/"></router-link> to app for the default route
+- Example of header/main css to get header centered:
+
+```
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fire Sans, sans-serif';
+
+  &::selection {
+    background: transparentize(#42B883, 0.5);
+  }
+}
+
+body {
+  background-color: #35495e;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px 16px;
+  background-color: #2c3D4E;
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.1);
+
+  h1 {
+    color: #FFF;
+    font-size: 28px;
+
+    span {
+      color: #42B883
+    }
+  }
+
+}
+</style>
+```
--- MovieApp/vue-movie-picker/src/views/HomeView.vue
+++ MovieApp/vue-movie-picker/src/views/HomeView.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
-    Home
+    <div class="feature-card">
+      <router-link to="/movie/tt0409591"></router-link>
+      <img
+        src="https://m.media-amazon.com/images/M/MV5BZmQ5NGFiNWEtMmMyMC00MDdiLTg4YjktOGY5Yzc2MDUxMTE1XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg"
+        alt="Naruto Poster" class="featured-img" />
+      <div class="detail">
+        <h3>Naruto</h3>
+        <p>Naruto Uzumaki, a mischievous adolescent ninja, struggles as he searches for recognition and dreams of becoming
+          the Hokage, the village's leader and strongest ninja.</p>
+      </div>
+      <router-link />
+    </div>
   </div>
 </template>
 
@@ -11,3 +22,37 @@ export default {
 
 }
 </script>
+
+<style lang="scss">
+.home {
+  .feature-card {
+    position: relative;
+
+    .featured-img {
+      display: block;
+      width: 100%;
+      height: 300px;
+      object-fit: cover;
+
+      position: relative;
+      z-index: 0;
+    }
+
+    .detail {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.6);
+      padding: 16px;
+
+      z-index: 1;
+
+      h3 {
+        color: #fff;
+        margin-bottom: 16px;
+      }
+    }
+  }
+}
+</style>
