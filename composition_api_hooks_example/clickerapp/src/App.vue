985bb2b3ac836b96f660be1b3dcedf22475000d5
John Stuby <johnstuby7@gmail.com>
2023-11-07 13:29:12 -0700

    created a vue example to use the composition api, removed all unneeded code, added in ref, and am starting to create a counter

---
+++ composition_api_hooks_example/clickerapp/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h2>{{ count }}</h2>
+</template>
+
+<script>
+import { ref } from "vue"
+export default {
+  setup() {
+    const count = ref(0);
+
+    return {
+      count
+    }
+  }
+}
+</script>
+
+<style></style>
