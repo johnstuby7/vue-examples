16bf7792f28cae56bd3b1afcdf2ecdfc63f828ef
John Stuby <johnstuby7@gmail.com>
2023-11-08 11:05:07 -0700

    Added in firebase authentication to the application

--- firebase_example/firebase_example/src/main.js
+++ firebase_example/firebase_example/src/main.js
@@ -1,14 +1,28 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-app.use(createPinia())
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA1_zdw8DwKDDt_eg4Elk_yEh3ccqbbE8s',
+  authDomain: 'fir-demo-4cf53.firebaseapp.com',
+  projectId: 'fir-demo-4cf53',
+  storageBucket: 'fir-demo-4cf53.appspot.com',
+  messagingSenderId: '742307362897',
+  appId: '1:742307362897:web:29a5776a260f956ed5e070'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+
 app.use(router)
 
 app.mount('#app')
--- firebase_example/firebase_example/src/router/index.js
+++ firebase_example/firebase_example/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/Home.vue'
+import Register from '../views/Register.vue'
+import Signin from '../views/Signin.vue'
+import Feed from '../views/Feed.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +13,19 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
+      path: '/sign-in',
+      name: 'Signin',
+      component: Signin
+    },
+    {
+      path: '/feed',
+      name: 'Feed',
+      component: Feed
     }
   ]
 })
